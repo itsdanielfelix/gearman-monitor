5ba72ad87e8949f09a341ff9854080864c6a8154
Daniel Felix <danielfelix1995@gmail.com>
2026-04-14 07:21:17 +0530

    Add exit functionality with 'q' and 'Escape' key support

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import { createCliRenderer, TextAttributes } from "@opentui/core";
-import { createRoot } from "@opentui/react";
+import { createRoot, useKeyboard, useRenderer } from "@opentui/react";
 
 function App() {
+  const renderer = useRenderer();
+  
+  useKeyboard((key) => {
+    if (key.name === "q" || key.name === "escape") {
+      renderer.destroy();
+    }
+  });
+
   return (
     <box alignItems="center" justifyContent="center" flexGrow={1}>
       <box justifyContent="center" alignItems="flex-end">
         <ascii-font font="tiny" text="OpenTUI" />
         <text attributes={TextAttributes.DIM}>What will you build?</text>
+        <text attributes={TextAttributes.DIM} marginTop={1}>
+          Press 'q' or ESC to exit
+        </text>
       </box>
     </box>
   );
